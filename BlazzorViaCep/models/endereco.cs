6ebc8f6645faa9b47dd48022f5551dc24b6a695c
Samuel <87245037+samucaeld2@users.noreply.github.com>
2025-10-26 20:58:40 -0300

    Add files via upload

---
+++ BlazzorViaCep/models/endereco.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazzorViaCep.models
+{
+    public class endereço
+    {
+         public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string unidade { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string estado { get; set; }
+        public string regiao { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+        public string ddd { get; set; }
+        public string siafi { get; set; }
+    }
+}
